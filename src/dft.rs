a45e589791b7a1251bfb757a537e10c820a9b751
Robert Kiel <robert@gelato.digital>
2025-02-06 20:27:16 +0100

    wip

--- src/dft.rs
+++ src/dft.rs
@@ -3,6 +3,6 @@ pub mod ntt;
 pub trait DFT<O> {
     fn forward_inplace(&self, x: &mut [O]);
     fn forward_inplace_lazy(&self, x: &mut [O]);
-    fn backward_inplace(&self, x: &mut[O]);
-    fn backward_inplace_lazy(&self, x: &mut[O]);
+    fn backward_inplace(&self, x: &mut [O]);
+    fn backward_inplace_lazy(&self, x: &mut [O]);
 }
--- src/dft/ntt.rs
+++ src/dft/ntt.rs
@@ -1,9 +1,8 @@
 use crate::dft::DFT;
-use mod_exp::mod_exp;
 use rand::{self, Rng};
 
 pub enum Prime {
-    /// Any prime <= 2^64
+    /// Any prime <= 2^61
     Generic,
     /// Any prime <= 2^51
     /// The implementation uses floating point arithmetic
@@ -17,23 +16,40 @@ pub struct Table<O> {
     q: O,
     /// n-th root of unity
     psi: O,
+    n: O,
+    powers_psi_bo: Vec<u64>,
+    powers_psi_inv_bo: Vec<u64>,
 }
 
 impl Table<u64> {
     pub fn new() -> Self {
-        Self {
+        let mut res = Self {
             q: 0x1fffffffffe00001u64,
             psi: 0x15eb043c7aa2b01fu64, //2^17th root of unity
-        }
+            n: 2u64.pow(16),
+            powers_psi_bo: Vec::with_capacity(2usize.pow(16)),
+            powers_psi_inv_bo: Vec::with_capacity(2usize.pow(16)),
+        };
+
+        res.with_precomputes();
+
+        res
     }
 
     pub fn new_simple() -> Self {
         // Values taken from https://eprint.iacr.org/2024/585.pdf
-        Self {
+        let mut res = Self {
             q: 7681,
             // 2^3th root of unity
-            psi: 4121,
-        }
+            psi: 1925,
+            n: 2u64.pow(2),
+            powers_psi_bo: Vec::with_capacity(2usize.pow(3)),
+            powers_psi_inv_bo: Vec::with_capacity(2usize.pow(3)),
+        };
+
+        res.with_precomputes();
+
+        res
     }
 }
 
@@ -87,13 +103,14 @@ impl Table<u64> {
     }
 
     pub fn forward_inplace_core<const LAZY: bool>(&self, a: &mut [u64]) {
-        let N = 2 ^ 16;
+        let a_len = a.len();
+        let log_a = a_len.ilog2();
 
-        let mut t: u64 = N;
-        let mut m: u64 = 1;
+        let mut t = a_len;
+        let mut m = 1;
 
         loop {
-            if m >= N {
+            if m >= a_len {
                 break;
             }
 
@@ -103,14 +120,30 @@ impl Table<u64> {
                 let j_1 = 2 * i * t;
                 let j_2 = j_1 + t - 1;
 
-                let cap_s = self.mod_exp(self.psi, ((m + i) as u16).reverse_bits() as u64);
+                let cap_s = self.powers_psi_bo[(m + i) as usize];
 
                 for j in j_1..=j_2 {
                     let cap_u = a[j as usize];
-                    let cap_v = a[j as usize + t as usize] * cap_s;
+                    let cap_v = self.mul_reduce(a[j as usize + t as usize], cap_s);
 
-                    a[j as usize] = (cap_u + cap_v) % self.q;
-                    a[j as usize + t as usize] = (cap_u - cap_v) % self.q;
+                    let mut cap_u_add_cap_v = cap_u + cap_v;
+                    if cap_u_add_cap_v > self.q {
+                        cap_u_add_cap_v -= self.q;
+                    }
+                    a[j as usize] = cap_u_add_cap_v;
+
+                    let cap_u_sub_cap_v = match cap_u.overflowing_sub(cap_v) {
+                        (res, true) => {
+                            let (inner_res, overflow) = res.overflowing_add(self.q);
+
+                            assert!(overflow);
+
+                            inner_res
+                        }
+                        (res, false) => res,
+                    };
+
+                    a[j as usize + t as usize] = cap_u_sub_cap_v % self.q;
                 }
             }
 
@@ -118,22 +151,140 @@ impl Table<u64> {
         }
     }
 
-    pub fn backward_inplace_core<const LAZY: bool>(&self, a: &mut [u64]) {}
+    pub fn backward_inplace_core<const LAZY: bool>(&self, a: &mut [u64]) {
+        let a_len = a.len();
+
+        let mut t = 1;
+        let mut m: u64 = a_len as u64;
+
+        loop {
+            if m == 1 {
+                break;
+            }
+
+            let mut j_1 = 0;
+            let h = m / 2;
+
+            for i in 0..h {
+                let j_2 = j_1 + t - 1;
+                let cap_s = self.powers_psi_inv_bo[(h + i) as usize];
+
+                for j in j_1..=j_2 {
+                    let cap_u = a[j];
+                    let cap_v = a[j + t];
+
+                    let mut cap_u_add_cap_v = cap_u + cap_v;
+
+                    if cap_u_add_cap_v > self.q {
+                        cap_u_add_cap_v -= self.q;
+                    }
+                    a[j] = cap_u_add_cap_v;
+
+                    let cap_u_sub_cap_v = match cap_u.overflowing_sub(cap_v) {
+                        (res, true) => {
+                            let (inner_res, overflow) = res.overflowing_add(self.q);
+
+                            assert!(overflow);
+
+                            inner_res
+                        }
+                        (res, false) => res,
+                    };
+
+                    a[j as usize + t as usize] = self.mul_reduce(cap_u_sub_cap_v, cap_s);
+                }
+                j_1 += 2 * t;
+            }
+
+            t *= 2;
+
+            m /= 2;
+        }
+
+        let n_inv = self.mod_exp(a_len as u64, self.q - 2);
+        for j in 0..a_len {
+            a[j as usize] = self.mul_reduce(a[j as usize], n_inv);
+        }
+    }
+
+    #[inline]
+    fn mul_reduce(&self, a: u64, b: u64) -> u64 {
+        ((a as u128 * b as u128) % self.q as u128) as u64
+    }
+
+    fn with_precomputes(&mut self) {
+        let psi_inv = self.mod_exp(self.psi, self.q - 2);
+
+        let mut tmp_psi = 1u64;
+        let mut tmp_psi_inv = 1u64;
+
+        self.powers_psi_bo.resize(self.n as usize, 0);
+        self.powers_psi_inv_bo.resize(self.n as usize, 0);
+
+        let log_n = self.n.ilog2();
+
+        for i in 0..self.n {
+            let reversed_index = bit_reverse(i, log_n) as usize;
+            self.powers_psi_bo[reversed_index] = tmp_psi;
+            self.powers_psi_inv_bo[reversed_index] = tmp_psi_inv;
+
+            tmp_psi = self.mul_reduce(tmp_psi, self.psi);
+            tmp_psi_inv = self.mul_reduce(tmp_psi_inv, psi_inv);
+        }
+    }
+
+    /// Assuming that m is prime and m-1 = 2^n * ...
+    fn find_nth_unity_root(&self, n: u64, m: u64) -> u64 {
+        let mut rand = rand::rng();
+
+        let mut tmp;
+        loop {
+            tmp = rand.random_range(2..m);
+
+            let g = self.mod_exp(tmp, (m - 1) / n);
+
+            match self.mod_exp(g, n / 2) {
+                1 => continue,
+                _ => break g as u64,
+            }
+        }
+    }
 }
 
-/// Assuming that m is prime and m-1 = 2^n * ...
-fn find_two_nth_unity_root(m: u64) -> u64 {
-    let mut rand = rand::rng();
+fn bit_reverse(n: u64, bits: u32) -> u64 {
+    let shift = (64 - bits) / 2;
+    (n << shift).reverse_bits() >> shift
+}
 
-    let mut tmp;
-    loop {
-        println!("iteration");
-        tmp = rand.random_range(2..m);
+fn u4_bit_reverse(n: u64) -> u64 {
+    match n {
+        0b0000 => 0b0000,
+        0b0001 => 0b1000,
+        0b0010 => 0b0100,
+        0b0011 => 0b1100,
+        0b0100 => 0b0010,
+        0b0101 => 0b1010,
+        0b0110 => 0b0110,
+        0b0111 => 0b1110,
+        0b1000 => 0b0001,
+        0b1001 => 0b1001,
+        0b1010 => 0b0101,
+        0b1011 => 0b1101,
+        0b1100 => 0b0011,
+        0b1101 => 0b1011,
+        0b1110 => 0b0111,
+        0b1111 => 0b1111,
+        _ => panic!("exceed bounds"),
+    }
+}
 
-        match mod_exp(tmp as u128, ((m - 1) / 2) as u128, m as u128) {
-            1 => continue,
-            _ => break tmp,
-        }
+fn u2_bit_reverse(n: u64) -> u64 {
+    match n {
+        0b00 => 0b00,
+        0b01 => 0b10,
+        0b10 => 0b01,
+        0b11 => 0b11,
+        _ => panic!("exceed bounds"),
     }
 }
 
@@ -141,19 +292,23 @@ fn find_two_nth_unity_root(m: u64) -> u64 {
 mod tests {
     use std::u128;
 
+    use rand::Rng;
+
     use crate::dft::DFT;
 
-    use super::{find_two_nth_unity_root, Table};
+    use super::Table;
 
     trait NaiveImplementation {
-        fn forward_naive(&self, a: &mut [u64], omega: u64);
+        fn forward_naive(&self, a: &mut [u64]);
 
-        fn backward_naive(&self, a: &mut [u64], omega: u64);
+        fn backward_naive(&self, a: &mut [u64]);
     }
 
     impl NaiveImplementation for Table<u64> {
-        fn forward_naive(&self, a: &mut [u64], omega: u64) {
+        fn forward_naive(&self, a: &mut [u64]) {
             let a_len = a.len();
+
+            let omega = self.mod_exp(self.psi, 2);
 
             let mut out = Vec::<u64>::with_capacity(a_len);
             out.resize(a_len, 0);
@@ -164,8 +319,9 @@ mod tests {
 
                     let omega_i_j = self.mod_exp(omega, i_j);
 
-                    out[j] = out[j]
-                        + ((a[i] as u128 * omega_i_j as u128) % self.q as u128) as u64 % self.q;
+                    out[j] = (out[j]
+                        + ((a[i] as u128 * omega_i_j as u128) % self.q as u128) as u64)
+                        % self.q;
                 }
             }
 
@@ -174,8 +330,10 @@ mod tests {
             }
         }
 
-        fn backward_naive(&self, a: &mut [u64], omega: u64) {
+        fn backward_naive(&self, a: &mut [u64]) {
             let a_len = a.len();
+
+            let omega = self.mod_exp(self.psi, 2);
 
             let mut out = Vec::<u64>::with_capacity(a_len);
             out.resize(a_len, 0);
@@ -217,14 +375,39 @@ mod tests {
     fn test_simple_case() {
         let table = Table::<u64>::new_simple();
 
-        let mut a = [1, 2, 3, 4];
+        let mut a = [1001, 2002, 3003, 4004];
         table.forward_inplace(&mut a);
 
         println!("ntt {a:?}");
 
-        // table.backward_inplace(&mut a);
+        let mut a_naive = [1, 2, 3, 4];
+
+        table.forward_naive(&mut a_naive);
+
+        println!("ntt naive {a_naive:?}");
+
+        table.backward_inplace(&mut a);
 
         println!("intt {a:?}");
+    }
+
+    #[test]
+    fn test_normal_case() {
+        let mut rng = rand::rng();
+        let table = Table::<u64>::new();
+
+        let mut a = [0u64; 2u64.pow(16) as usize];
+
+        for i in 0..a.len() {
+            a[i] = rng.random_range(0..2 ^ 61);
+        }
+
+        let a_clone = a.clone();
+        table.forward_inplace(&mut a);
+
+        table.backward_inplace(&mut a);
+
+        assert_eq!(a_clone, a);
     }
 
     // #[test]
@@ -239,6 +422,14 @@ mod tests {
 
     #[test]
     fn test_find_nth_unity_root() {
-        println!("{}", find_two_nth_unity_root(7681));
+        let foo = Table::new();
+        println!("{}", foo.find_nth_unity_root(8, 7681));
+    }
+
+    #[test]
+    fn test_arithmetics() {
+        let res = 3u64.overflowing_sub(5u64);
+
+        println!("{:?}", res.0 + 5);
     }
 }
